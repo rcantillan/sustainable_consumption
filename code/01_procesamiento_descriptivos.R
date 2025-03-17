70553329667675dc0e4e733176508de3ac76f5fa
Valentina González <vale.spgm@gmail.com>
2025-03-17 01:30:32 -0300

    local

--- code/01_procesamiento_descriptivos.R
+++ code/01_procesamiento_descriptivos.R
@@ -1,6 +1,6 @@
 #==================================================  
-# Project: Sustainable Consumption
-# Description:Preparación de datos descriptivos
+# Project: Sustainablec Consumption
+# Description:Descriptivos
 #==================================================
 
 # Cargar librerías
@@ -8,13 +8,81 @@ library(here)
 library(tidyverse)
 library(haven)
 library(psych)
-
+library(dplyr)
 
 # Cargar datos
 datos <- read_sav(here("datos", "BBDD Consolidada Maule UCM 2810.sav"))
 
-# Escala de Modo de Vida Imperial (MVI)
-vars_mvi <- c("P7_2", "P7_4", "P7_5", "P4_2", "P4_4")
+covar <- dplyr::select(datos, M1,M2,M5,M61)
 
-# Escala de Reflexividad Ambiental Antropocéntrica (RAA)
-vars_raa <- c("P7_1", "P7_3", "P4_1", "P4_3")
+
+
+# Renombrar 
+covar <- covar %>%
+  rename(
+    region = M1,  # región 1=maule, 2=rm
+    sexo = M2,) # sexo 1=hombre, 2=mujer
+
+
+# Recodificar 
+covar <- covar %>%
+  mutate(
+    edad = case_when(
+      M5 >= 18 & M5 <= 24 ~ 1,
+      M5 >= 25 & M5 <= 34 ~ 2,
+      M5 >= 35 & M5 <= 44 ~ 3,
+      M5 >= 45 & M5 <= 54 ~ 4,
+      M5 >= 55 & M5 <= 64 ~ 5,
+      M5 >= 65 ~ 6,
+      TRUE ~ NA_real_
+    ),
+    edad = factor(edad, 
+                  levels = c(1, 2, 3, 4, 5, 6),
+                  labels = c("18 a 24 años", "25 a 34 años", "35 a 44 años", 
+                             "45 a 54 años", "55 a 64 años", "65 años o más")))
+
+
+# Recodificar educ
+covar <- covar %>%
+  mutate(
+    educacion = case_when(
+      M61 %in% c(1, 2, 3) ~ 1,   #sin educacion hasta básica completa
+      M61 %in% c(4, 5) ~ 2,   #educación media comp o incom
+      M61 %in% c(6, 7) ~ 3,   #educación técnica comp o incom
+      M61 %in% c(8, 9) ~ 4,  #educación universitaria comp o incom
+      TRUE ~ NA_real_
+    ),
+    educacion = factor(educacion, 
+                       levels = 1:4,
+                       labels = c("Educación básica o menos", 
+                                  "Educación media", 
+                                  "Educación técnica", 
+                                  "Educación universitaria")))
+
+
+covar <- dplyr::select(covar, sexo, edad, educacion, region)
+  
+
+load("objects/vars_modelo.RData")
+
+# Combinar variables y covariables en un solo dataframe
+datos_completos <- cbind(vars_modelo, covar)
+
+
+fc <- cbind(P7_2, P7_4, P7_5, P4_2, P4_4, 
+           P7_1, P7_3, urgenciacrisis, 
+           actividadeshumanas) ~ sexo + edad + educacion + region
+
+
+# N de clases 
+lca2 <- poLCA(fc, vars_modelo, nclass = 2, 
+              maxiter = 1000, nrep = 10)
+lca3 <- poLCA(fc, vars_modelo, nclass = 3,  # modelo con mejor ajuste
+              maxiter = 1000, nrep = 10)
+lca4 <- poLCA(fc, vars_modelo, nclass = 4, 
+              maxiter = 1000, nrep = 10)
+lca5 <- poLCA(fc, vars_modelo, nclass = 5, 
+              maxiter = 1000, nrep = 10)
+lca6 <- poLCA(fc, vars_modelo, nclass = 6, 
+              maxiter = 1000, nrep = 10)
+
--- code/02_LCA.R
+++ code/02_LCA.R
@@ -3,10 +3,104 @@
 # Description:Análisis de Clases Latentes
 #==================================================
 
-
-
-
 # Cargar librerías
 library(here)
 library(tidyverse)
+library(haven)
+library(psych)
 library(poLCA)
+library(dplyr)
+
+
+# Cargar datos
+datos <- read_sav(here("datos", "BBDD Consolidada Maule UCM 2810.sav"))
+
+# Ver la estructura de los datos
+str(datos)
+
+# Escala de Modo de Vida Imperial (MVI)
+vars_mvi <- c("P7_2", "P7_4", "P7_5", "P4_2", "P4_4")
+
+# Escala de Reflexividad Ambiental Antropocéntrica (RAA)
+vars_raa <- c("P7_1", "P7_3", "P4_1", "P4_3")
+
+
+#  Recodificar P4_1 y P4_3 para mantener coherencia con las escalas
+datos <- datos %>%
+  mutate(
+    urgenciacrisis = 5 - P4_1, 
+    actividadeshumanas = 5 - P4_3
+  )
+
+#  Variables seleccionadas 
+vars_modelo <- dplyr::select(datos,P7_2, P7_4, P7_5, P4_2, P4_4, 
+                      P7_1, P7_3, urgenciacrisis, actividadeshumanas)
+
+
+# Dicotomizar las variables originales directamente a categorías 1 y 2
+vars_modelo <- vars_modelo %>%
+  mutate(across(everything(), 
+                ~ifelse(is.na(.), NA, ifelse(. >= 3, 2, 1))))
+
+#guardar 
+save(vars_modelo, file = "objects/vars_modelo.RData")
+
+
+#________________________Clases latentes
+set.seed(1234)
+
+# Clases latentes sin restricciones
+f<- cbind(P7_2, P7_4, P7_5, P4_2, P4_4, 
+          P7_1, P7_3, urgenciacrisis, 
+          actividadeshumanas) ~ NULL
+
+
+# N de clases 
+lca2 <- poLCA(f, vars_modelo, nclass = 2, 
+              maxiter = 1000, nrep = 10)
+lca3 <- poLCA(f, vars_modelo, nclass = 3,  # modelo con mejor ajuste
+              maxiter = 1000, nrep = 10)
+lca4 <- poLCA(f, vars_modelo, nclass = 4, 
+              maxiter = 1000, nrep = 10)
+lca5 <- poLCA(f, vars_modelo, nclass = 5, 
+              maxiter = 1000, nrep = 10)
+lca6 <- poLCA(f, vars_modelo, nclass = 6, 
+              maxiter = 1000, nrep = 10)
+
+#Comparar los modelos
+modelos <- list(lca2, lca3, lca4, lca5, lca6)
+n_clases <- 2:6
+
+# Crear tabla de comparación
+resultados <- data.frame(
+  Clases = n_clases,
+  BIC = sapply(modelos, function(x) x$bic),
+  AIC = sapply(modelos, function(x) x$aic),
+  Log_likelihood = sapply(modelos, function(x) x$llik)
+)
+
+# Ver resultados
+print(resultados)
+
+# probabilidades condicionales
+modelo_3 <- lca3
+
+
+# Extraer probabilidades condicionales
+probs_clase <- modelo_3$probs
+print(probs_clase)
+
+# Extraer tamaño de las clases
+class_sizes <- modelo_3$P
+
+#============================================================
+# Categoría 1: Desacuerdo (valores originales 1-2)
+# Categoría 2: Acuerdo (valores originales 3-4)
+
+#Clase 1 (37.8% de la muestra): alta adherencia a modo de vida imperial, moderada reflexibidad ambiental 
+#Clase 2 (53.5% de la muestra - mayoría): moderada adherencia a modo de vida imperial, baja reflexividad ambiental
+#Clase 3 (8.7% de la muestra): moderada adherencia a modo de vida imperial, alta reflexividad ambiental
+
+
+
+
